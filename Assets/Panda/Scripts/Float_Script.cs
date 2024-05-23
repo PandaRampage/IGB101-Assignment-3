bd35473da2c4d04608bc280a42981ed188265de4
PandaRampage <johenlu10@gmail.com>
2024-05-23 20:23:26 +1000

    Added scripts and fixed camera

---
+++ Assets/Panda/Scripts/Float_Script.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Float_Script : MonoBehaviour
+{
+
+    public float x;
+    public float z;
+    public float y;
+    public float amp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(x, Mathf.Sin(Time.time) *amp +y, z);
+    }
+}
