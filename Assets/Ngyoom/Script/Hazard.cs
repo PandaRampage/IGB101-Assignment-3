ffacc93493a7b4406a4c5178bfde50b12be41d3b
ngyoomuni <n11242779@qut.edu.au>
2024-05-31 02:18:14 +1000

    Created Hazard Script (you'll need to duplicate and edit the script if using)

---
+++ Assets/Ngyoom/Script/Hazard.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hazard : MonoBehaviour
+{
+    GameManager gameManager;
+    public string currentLevel;
+
+    private void OnTriggerEnter(Collider otherObject)
+    {
+        if(otherObject.transform.tag == "Player")
+        SceneManager.LoadScene("Ngyoom Scene");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
